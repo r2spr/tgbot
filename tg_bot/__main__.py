a6191d99ab594847a1bfbe9ea33b5999f58ef921
r2spr <105548769+r2spr@users.noreply.github.com>
2022-08-29 01:27:17 +0530

    Update __main__.py

--- tg_bot/__main__.py
+++ tg_bot/__main__.py
@@ -20,12 +20,12 @@ from tg_bot.modules.helper_funcs.misc import paginate_modules
 
 PM_START_TEXT = """
 Hi {}, my name is {}! I'm a group manager bot maintained by [this wonderful person](tg://user?id={}).
-I'm built in python3, using the python-telegram-bot library, and am fully [opensource](https://github.com/PaulSonOfLars/tgbot) you can find what makes me tick \
-[here](github.com/SpEcHiDe/PSonOfLars_Marie)!
+I'm built in python3, using the python-telegram-bot library, and am fully [Main Channel](https://t.me/HombaleCinemas) you can find what makes me tick \
+[Support Group](https://t.me/HombaleCinemasChat)!
 
 You can find the list of available commands with /help.
 
-Using standard [Marie](http://telegram.dog/BanhammerMarie_bot) is always gonna be easier.
+Using standard [support group](https://t.me/HombaleCinemasChat) is always gonna be easier.
 Perks:
 - latest features
 - it's free
